8c46dfcd9d4f4a67e63dc376e514125b8c39f65a
Rohith Palakirti <rohithkp99@gmail.com>
2022-10-17 01:49:44 +0000

    Add location data fetching API script

--- get_data.py
+++ get_data.py
@@ -7,27 +7,29 @@ current_time = now.strftime("%d/%m/%Y %H:%M:%S")
 print(current_time, ": Running owntracks data fetching script")
 
 server = 'http://152.67.97.63:8083/api/0/'
+path = '../librephotos-frontend/src/owntracks-data/'
 res = requests.get(server+'list/')
 
 data = res.json()
-with open('users_list.json', 'w', encoding='utf-8') as list_file:
+with open(path+'users_list.json', 'w', encoding='utf-8') as list_file:
     json.dump(data, list_file, ensure_ascii=False, indent=4)
 
 for results in data:
     for user in data[results]:
         params = {
             'user' : user,
-            'device': user+'phone'
+            'device': user+'phone',
+            'from': '2015-09-01'
         }
         l = requests.get(server+'locations', params=params)
         locations = l.json()
 
-        with open('%s_data.json' % user, 'w', encoding='utf-8') as f:
+        with open(path+'%s_data.json' % user, 'w', encoding='utf-8') as f:
             json.dump(locations, f, ensure_ascii=False, indent=4)
 
 res_last = requests.get(server+'last/')
 last = res_last.json()
-with open('last.json', 'w', encoding='utf-8') as last_file:
+with open(path+'last.json', 'w', encoding='utf-8') as last_file:
     json.dump(last, last_file, ensure_ascii=False, indent=4)
 
-time.sleep(86400)
+time.sleep(8640)
